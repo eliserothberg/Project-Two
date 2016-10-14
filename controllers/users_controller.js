9031b6a6dd844098d95964a903ee70585e92d762
eliserothberg <rothberg.elise@gmail.com>
2016-10-14 02:59:51 -0700

    table adjustment, adding JAWSDB/heroku

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,7 +3,6 @@ var models  = require('../models');
 var express = require('express');
 var router  = express.Router();
 
-//this is the users_controller.js file
 router.get('/new', function(req,res) {
 	res.render('users/new');
 });
@@ -18,7 +17,6 @@ router.get('/sign-out', function(req,res) {
   })
 });
 
-
 // login
 router.post('/login', function(req, res) {
   models.User.findOne({
@@ -29,33 +27,18 @@ router.post('/login', function(req, res) {
 			res.redirect('/users/sign-in')
 		}
 
-		// Solution:
-		// =========
-		// Use bcrypt to compare the user's password input
-		// with the hash stored in the user's row. 
-		// If the result is true, 
+		// bcrypt compares user's password input with stored hash
     bcrypt.compare(req.body.password, user.password_hash, function(err, result) {
-        // if the result is true (and thus pass and hash match)
         if (result == true){
-
-        	// save the user's information 
-					// to req.session, as the comments below show 
-
-					// so what's happening here?
-					// we enter the user's session by setting properties to req.
-
-					// we save the logged in status to the session
+					// we save the logged in status user id and email to the session
           req.session.logged_in = true;
-          // the username to the session
 					req.session.username = user.username;
-					// the user id to the session
           req.session.user_id = user.id;
-          // and the user's email.
           req.session.user_email = user.email;
 
           res.redirect('/');
         }
-        // if the result is anything but true (password invalid)
+        // if password invalid
         else{
         	// redirect user to sign in
 					res.redirect('/users/sign-in')
@@ -76,36 +59,22 @@ router.post('/create', function(req,res) {
 			res.send('we already have an email or username for this account')
 		}else{
 
-			// Solution:
-			// =========
-
-			// Using bcrypt, generate a 10-round salt,
-			// then use that salt to hash the user's password.
+			// Use bcrypt to generate a 10-round salt and then hash the user's password.
 			bcrypt.genSalt(10, function(err, salt) {
 					bcrypt.hash(req.body.password, salt, function(err, hash) {
 						
-						// Using the User model, create a new user,
-						// storing the email they sent and the hash you just made
+						// create new user and store info
 						models.User.create({
 							email: req.body.email,
 							password_hash: hash
 						})
-						// In a .then promise connected to that create method,
-						// save the user's information to req.session
-						// as shown in these comments
 						.then(function(user){
 
-
-							// so what's happening here?
-							// we enter the user's session by setting properties to req.
-
-							// we save the logged in status to the session
+							//enter the user's session by setting properties to req.
+							// and save the logged in status to the session
 		          req.session.logged_in = true;
-		          // the username to the session
 							req.session.username = user.username;
-							// the user id to the session
 		          req.session.user_id = user.id;
-		          // and the user's email.
 		          req.session.user_email = user.email;
 
 		          // redirect to home on login
