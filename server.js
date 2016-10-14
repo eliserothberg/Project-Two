cf917b623466fc272ea3d141aac0d0c6adeff717
eliserothberg <rothberg.elise@gmail.com>
2016-10-14 15:10:32 -0700

    adjusting for heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // ============
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser'); // for working with cookies
 var bodyParser = require('body-parser');
@@ -58,8 +57,6 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-// uncomment after placing favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
