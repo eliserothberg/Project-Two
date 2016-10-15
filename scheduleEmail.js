99ea75ae1472de5d65a712598eb35e1513e8e5c0
eliserothberg <rothberg.elise@gmail.com>
2016-10-14 19:34:02 -0700

    heroku

--- scheduleEmail.js
+++
@@ -1,7 +0,0 @@
-
-//var ?? = require('the email program');
-
-function email_function_will_go_here() {
-    console.log('I take care of the email functionality!');
-}
-email_function_will_go_here();
