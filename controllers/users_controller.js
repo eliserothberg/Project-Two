0d3d8606f124d6c105355fdff17b38e2682f373a
eliserothberg <rothberg.elise@gmail.com>
2016-10-16 13:21:33 -0700

    small adjustment to user_controller and scheduleEmail

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -38,12 +38,9 @@ console.log('at the bcrypt');
         	console.log(req.body.email);
 					// we save the logged in status user id and email to the session
 	        req.session.logged_in = true;
-<<<<<<< HEAD
-			// req.session.username = req.body.email;
-=======
+
 			req.session.username = req.body.email;
           // req.session.username = user.username;
->>>>>>> elise
 	        req.session.user_id = user.id;
 	        userId=user.id;
 	        req.session.email = user.email;
