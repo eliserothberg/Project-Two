0a35bc4bb14c1a9fba24b0d87fdf69fb79eb4e82
eliserothberg <rothberg.elise@gmail.com>
2016-10-17 23:51:33 -0700

    more non-working tests

---
+++ test/functions_test.js
@@ -0,0 +1,128 @@
+// var expect = require('chai').expect;
+var should = require('chai').should;
+
+var Login = require('../controllers/users_controller.js')
+//should allow all registered users access to the site
+describe('Login', function() {
+	it('should allow user to enter their name'
+	// 	, function(done) {
+ //  	chai.request(server)
+	//     .get('/')
+	//     .end(function(err, res){
+	//       res.should.have.status(200);
+	//       done();
+	//     });
+	// }
+	);  
+	it('should allow returning user to enter their password');
+  it('should allow returning user to submit information');
+  it('should tell new user there is no record of them in the system');
+  it('should redirect returning user to gift entry page');
+});
+
+var Register = require('../controllers/users_controller.js')
+//should allow all registered users access to the site
+describe('Register', function() {
+	it('should allow new user to enter their name' 
+	// 	, function(done) {
+	//   chai.request(server)
+	//     .get('/')
+	//     .end(function(err, res){
+	//       res.should.have.status(200);
+	//       done();
+	//     });
+	// }
+	);  
+	it('should allow new user to enter their password');
+  it('should allow new user to submit information');
+  it('should redirect new user to gift entry page');
+  it('should tell user we already have an email or username for this account if already registered')
+  it('should redirect new user to gift entry page');
+
+});
+
+var enterEvent = require('../controllers/users_controller.js')
+//should allow all registered users access to the site
+describe('enterEvent', function() {
+	it('should allow user to enter a gift name'
+		// , function(done) {
+	 //  chai.request(server)
+	 //    .get('/')
+	 //    .end(function(err, res){
+	 //      res.should.have.status(200);
+	 //      done();
+	 //    });
+		// }
+		);  
+	it('should allow user to enter a recipient name');
+  it('should allow user to enter a max price for the gift');
+  it('should allow user to enter an event date');
+});
+
+// var Gift = require('../models/gift.js')
+
+// describe('?', function() {
+// it('should ?', function(done) {
+//   chai.request(server)
+//     .get('/events')
+//     .end(function(err, res){
+//       res.should.have.status(200);
+//       done();
+//     });
+// });  
+// it('should ?? GET');
+//   it('should ?? POST');
+//   it('should ?? PUT');
+//   it('should ?? DELETE');
+// });
+
+// var Letter = require('../lib/letter');
+
+// describe("Letter", function() {
+// 	it("is created with a letter", function() {
+// 		var l = new Letter('g');
+// 		expect(l.letter).to.equal('g');
+// 	});
+
+// 	it("is not guessed by default", function() {
+// 		var l = new Letter('g');
+// 		expect(l.guessed).to.equal(false);
+// 	});
+
+// 	describe('toString', function() {
+// 		it("returns a placeholder if it is not guessed", function() {
+// 			var l = new Letter('g');
+// 			l.guessed = false;
+// 			expect(l.toString()).to.equal('_');
+// 		});
+
+// 		it("returns the letter if it is guessed", function() {
+// 			var l = new Letter('g');
+// 			l.guessed = true;
+// 			expect(l.toString()).to.equal('g');
+// 		});
+// 	});
+
+// 	describe('guess', function() {
+// 		it("sets the letter as guessed if it's the right letter", function() {
+// 			var l = new Letter('g');
+// 			l.guess('g');
+// 			expect(l.guessed).to.equal(true);
+// 		});
+
+// 		it("does not set the letter as guessed for the wrong letter", function() {
+// 			var l = new Letter('g');
+// 			l.guess('a');
+// 			expect(l.guessed).to.equal(false);
+// 		});
+
+// 		it("ignores the guess if the letter is already guessed", function() {
+// 			var l = new Letter('g');
+// 			l.guess('g');
+// 			expect(l.guessed).to.equal(true);
+// 			l.guess('a');
+// 			expect(l.guessed).to.equal(true);
+// 		})
+// 	});
+// })
+
--- test/signup_test.js
+++ test/signup_test.js
@@ -8,36 +8,7 @@ var should = require('chai').should();
 
 describe('eMinder', function() {
   this.timeout(10000);
-// it('should log in', function(done) {
-//     var nightmare = new Nightmare({
-//       show: true
-//     });
-//   nightmare
-//   // Visit login page
-//   .goto('https://pure-scrubland-84099.herokuapp.com//users/sign-in')
-//   // Enter username 
-//   .type('#item', 'bob@gmail.com') 
-//   // Enter password 
-//   .type('#pass', 'bob')
-//   // Take a screenshot of the login form.
-//   .screenshot('login.png')
-//   // Click login button. Always check if you've done this where necesssary!
-//   //   It's easy to forget.
-//   .click('#submit')
-//   // Wait until the element containing videos I can select is rendered
-//   // .wait('div.course-list-alt')
-//   // // Scroll down a few hundred pixels.
-//   // .scrollTo(500, 0)
-//   // // Take a screenshot and save it to the current directory.
-//   // .screenshot('eventScreen.png')
-//   // // Make sure there's more than one video to choose from.
-//   // .evaluate(function () {
-//   //    return document.querySelectorAll('div#form-group.aname').length;
-//   // })
-//   // 
-//   .end()
 
-// });
   it('should go to log in page', function(done) {
     var nightmare = new Nightmare({
       show: true
@@ -70,10 +41,10 @@ describe('eMinder', function() {
       .goto('https://pure-scrubland-84099.herokuapp.com/users/sign-in/')
       .wait('a[href*="https://pure-scrubland-84099.herokuapp.com/users/sign-in/"]')
       .click('a[href*="https://pure-scrubland-84099.herokuapp.com/users/sign-in/"]')
-      .wait('#em_login')
-      .type('#em_login', 'bob@gmail.com')
-      .type('#pass_login', 'bob')
-      .click('#submit_login')
+      .wait('#em_logging')
+      .type('#em_logging', 'bob@gmail.com')
+      .type('#pass_logging', 'bob')
+      .click('#submit_logging')
       // .wait('div#form-group.aname')
       // .evaluate(function() {
       //   return document.querySelectorAll('div#form-group.aname').length;
@@ -91,8 +62,4 @@ describe('eMinder', function() {
         console.error("Log in not working");
       });
   });
-
-  // it('should ', function () {
-  //   throw new Error('Failed on purpose, just to make the Mocha output more interesting.');
-  // });
 });
