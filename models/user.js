f3d94b89110b488d4c67d18348004ce6f4d5c42d
eliserothberg <rothberg.elise@gmail.com>
2016-10-21 22:25:01 -0700

    cleanup

--- models/user.js
+++ models/user.js
@@ -11,33 +11,10 @@ module.exports = function(sequelize, DataTypes) {
     password_hash: DataTypes.STRING,
     active: DataTypes.BOOLEAN
   }, {
-    // don't use camelcase for automatically added attributes but underscore style
-    // underscored: true,
-
-    // disable the modification of tablenames
-    // freezeTableName: true,
-
-    // define the table's name
-    // tableName: 'users',
 
     classMethods: {
       associate: function(models) {
         User.hasMany(models.Event, {foreignKey: 'user_id'});
-        // User.hasMany(models.Event, {onDelete: 'cascade', hooks:true});
-        // User.hasMany(models.Gift, {
-        //   onDelete: "CASCADE",
-        //   hooks: true,
-        //   foreignKey: {
-        //     allowNull: false
-        //   }
-        // })
-        // User.hasMany(models.Event, {
-        //   onDelete: "CASCADE",
-        //   hooks: true,
-        //   foreignKey: {
-        //     allowNull: false
-        //   }
-        // })
       }
     }
   })
